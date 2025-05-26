67b7cb9a33ec1bf915dc826982c6ad95fa166888
dzakialaqsha <dzakialaqsha1225@gmail.com>
2025-05-26 10:15:31 +0700

    Update get_emiten.py

--- data/utils/get_emiten.py
+++ data/utils/get_emiten.py
@@ -4,10 +4,10 @@ from rpy2.robjects.packages import importr
 import sys
 
 def get_emiten_list():
-  '''
-  Executes an R web scaper script
-  '''
-  try:
+    '''
+    Executes an R web scaper script
+    '''
+    try:
         ip = get_ipython()
         if ip is not None:
             ip.run_line_magic('load_ext', 'rpy2.ipython')
@@ -18,12 +18,13 @@ def get_emiten_list():
         print("Not in IPython environment. Skipping %load_ext.")
         print("Ensure rpy2 is installed (`pip install rpy2`) if running standalone.")
 
-  scraper_path = "/content/Portfolio-Tracker/data/utils/get_emiten.R" 
-
-  if os.path.exists(r_script_path):
-    try:
-      ro.r(f"source('{scraper_path}')")
-    except Exception as e:
-      print(f"Error sourcing R script '{scraper_path}': {e}")
-  else:
-    print(f"The file '{r_script_path}' DOES NOT exist.")
+    scraper_path = "/content/Portfolio-Tracker/data/utils/get_emiten.R"
+    if os.path.exists(scraper_path): 
+        print(f"Attempting to source R script: '{scraper_path}'...")
+        try:
+            ro.r(f"source('{scraper_path}')")
+            print(f"R script '{scraper_path}' sourced successfully!")
+        except Exception as e:
+            print(f"Error sourcing R script '{scraper_path}': {e}")
+    else:
+        print(f"Error: The file '{scraper_path}' DOES NOT exist at the specified path.") 
